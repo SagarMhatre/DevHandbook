34becf64a4d01e006aa553357e89f0f07b7f3d17
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2024-03-05 10:06:04 +0800

    Added endpoint to translate

--- docs/whisper/ReadMe.md
+++ docs/whisper/ReadMe.md
@@ -1,7 +1,16 @@
 Ref: https://lablab.ai/t/whisper-api-flask-docker
 
-docker build -t whisper-api .
+```sh
+docker build -t whisper-api:1.3 .
+docker run -p 5000:5000 --name my-whisper-api-13 --rm whisper-api:1.3
+```
 
-docker run -p 5000:5000 whisper-api
+```rest
+curl -F "file=@englishTestAudio.mp3" http://localhost:5000/whisper
+```
 
-curl -F "file=@testaudio.mp3" http://localhost:5000/whisper
+src - https://audio-lingua.ac-versailles.fr/spip.php?rubrique1&lang=en
+
+```rest
+curl -F "file=@french test audio - Elisa - le film WONKA.mp3" http://localhost:5000/whisper-translate
+```
--- docs/whisper/app.py
+++ docs/whisper/app.py
@@ -2,6 +2,7 @@ from flask import Flask, abort, request
 from tempfile import NamedTemporaryFile
 import whisper
 import torch
+import json
 
 # Check if NVIDIA GPU is available
 torch.cuda.is_available()
@@ -18,7 +19,8 @@ def hello():
 
 
 @app.route('/whisper', methods=['POST'])
-def handler():
+def handler():   
+
     if not request.files:
         # If the user didn't submit any files, return a 400 (Bad Request) error.
         abort(400)
@@ -34,13 +36,63 @@ def handler():
         # Write the user's uploaded file to the temporary file.
         # The file will get deleted when it drops out of scope.
         handle.save(temp)
+
         # Let's get the transcript of the temporary file.
-        result = model.transcribe(temp.name)
+        result = model.transcribe(temp.name) 
+        
+        # warnings.warn("FP16 is not supported on CPU; using FP32 instead")
+        # https://stackoverflow.com/a/75935598/2795764
+        # ,fp16=False )
+        # https://github.com/openai/whisper/discussions/301
+        # ,fp16=False , language='English')
+
+        pretty_json = json.dumps(result, indent=4)
+        print(pretty_json)
+
         # Now we can store the result object for this file.
         results.append({
             'filename': filename,
             'transcript': result['text'],
         })
+
+    # This will be automatically converted to JSON.
+    return {'results': results}
+
+
+@app.route('/whisper-translate', methods=['POST'])
+def translate():   
+
+    if not request.files:
+        # If the user didn't submit any files, return a 400 (Bad Request) error.
+        abort(400)
+
+    # For each file, let's store the results in a list of dictionaries.
+    results = []
+
+
+
+    # Loop over every file that the user submitted.
+    for filename, handle in request.files.items():
+        # Create a temporary file.
+        # The location of the temporary file is available in `temp.name`.
+        temp = NamedTemporaryFile()
+        # Write the user's uploaded file to the temporary file.
+        # The file will get deleted when it drops out of scope.
+        handle.save(temp)
+
+        # Whisper can also facilitate audio translation in other supported languages into English text
+        result = model.transcribe(temp.name, task = 'translate')
+
+        pretty_json = json.dumps(result, indent=4)
+        print(pretty_json)
+
+        # Now we can store the result object for this file.
+        results.append({
+            'filename': filename,
+            'transcript': result['text'],
+        })
+
+
 
     # This will be automatically converted to JSON.
     return {'results': results}
